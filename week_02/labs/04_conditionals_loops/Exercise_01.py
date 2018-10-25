31ac5f68d054bc1d27c57f1810b80cdb8520b23c
Patrikc Vargos <pvargos17@gmail.com>
2018-10-25 17:10:34 -0500

    completed

--- week_02/labs/04_conditionals_loops/Exercise_01.py
+++ week_02/labs/04_conditionals_loops/Exercise_01.py
@@ -6,9 +6,23 @@ Print the result.
 NOTE: We will be using the input() function. This is demonstrated below.
 
 '''
-number = input("Pick a number between 1 and 1,000,000 ")
+number = int(input("Pick a number between 1 and 1,000,000: "))
 
 if number % 2 == 0:
-    print("Even")
+    print(f"{number} is Even")
 else:
-    print("False")
+    print(f"{number} is Odd")
+
+
+# user_input = int(input("Enter a number between 1 and 1,000,000,000 \
+# to see whether is it odd or even: "))
+
+# #print the number the user inputed to the console
+# print(f"The user input is {user_input}")
+
+# #complete the rest of the code to determine whether it is odd or even
+# if user_input % 2 == 0:
+#     print(f"{user_input} is EVEN")
+# else:
+#     print(f"{user_input} is ODD")
+
--- week_02/labs/04_conditionals_loops/Exercise_03.py
+++ week_02/labs/04_conditionals_loops/Exercise_03.py
@@ -2,3 +2,6 @@
 Using a "for-loop", print out all even numbers from 1-100.
 
 '''
+for n in range(1,101):
+    if n % 2 == 0:
+        print(n)
