e8c67b06083f2bd290c42d1a9fa2e831ca082172
Patrikc Vargos <pvargos17@gmail.com>
2018-10-26 16:46:16 -0500

    completed

--- week_02/labs/04_conditionals_loops/Exercise_08.py
+++ week_02/labs/04_conditionals_loops/Exercise_08.py
@@ -2,3 +2,10 @@
 Demonstrate the use of the "break" statement to exit a loop.
 
 '''
+
+
+for n in range( 1, 101):
+    if n is 63:
+        break
+    else:
+        print(n)
--- week_02/labs/04_conditionals_loops/Exercise_09.py
+++ week_02/labs/04_conditionals_loops/Exercise_09.py
@@ -13,3 +13,6 @@ The output should look something like this:
 	10|20|30|40|50|60|70|80|90|100|
 
 '''
+
+for n in range( 1, 101):
+	if n
