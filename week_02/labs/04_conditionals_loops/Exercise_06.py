1b0f4cc5b3cde7d49ffd016699f8b71da73cedc5
Patrikc Vargos <pvargos17@gmail.com>
2018-10-26 16:37:59 -0500

    completed

--- week_02/labs/04_conditionals_loops/Exercise_06.py
+++ week_02/labs/04_conditionals_loops/Exercise_06.py
@@ -3,3 +3,21 @@ Using a "while" loop, find the sum of numbers from 1-100.
 Print the sum to the console.
 
 '''
+
+# def sumnum(int):
+#     sum1 = 0
+#     while int > 0:
+#         sum1 += num
+#         num = num -1
+#     print(sum1)
+# num = 100
+# print(sumnum(num))
+
+count = 0
+count_sum = 0
+
+while count <= 100:
+    count_sum += count
+    count += 1
+
+print(count_sum)
--- week_02/labs/04_conditionals_loops/Exercise_07.py
+++ week_02/labs/04_conditionals_loops/Exercise_07.py
@@ -2,3 +2,15 @@
 Use a "while" loop to print out every third number counting backwards from 1000 to 1.
 
 '''
+
+count = 1000
+iterration = 1
+while count >= 1:
+    if iterration == 3:
+        print(count)
+        iterration = 1
+    else:
+        iterration +=1
+    count = count - 1
+
+
