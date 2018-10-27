8265c64fdbba8a126bd7877e26ea98e77596ca50
Patrikc Vargos <pvargos17@gmail.com>
2018-10-27 16:23:13 -0500

    completed

--- week_03/labs/07_lists/Exercise_01.py
+++ week_03/labs/07_lists/Exercise_01.py
@@ -6,3 +6,14 @@ numbers in the list as well as the average.
 Print the results.
 
 '''
+user_input = input("Please enter 10 numbers: ").split()
+numbers = [float(num) for num in user_input]
+
+# calculating the sum of all numbers
+num_sum = 0
+for num in numbers:
+    num_sum += num
+print(num_sum)
+
+avg = sum(numbers) / len(numbers)
+print(avg)
--- week_03/labs/07_lists/Exercise_02.py
+++ week_03/labs/07_lists/Exercise_02.py
@@ -9,3 +9,15 @@ Print both.
 
 list_one = [0, 4, 6, 18, 25, 42, 100]
 list_two = [1, 4, 9, 24, 42, 88, 99, 100]
+
+same , different = [],[]
+for n in list_one:
+    if n in list_two:
+        same.append(n)
+    else:
+        different.append(n)
+
+different += [x for x in list_two if x not in list_one]
+
+print(same)
+print(different)
