a1025a5239f7dbcda688c882b22226bdccec3154
Patrikc Vargos <pvargos17@gmail.com>
2018-10-27 16:02:46 -0500

    complete

--- week_03/labs/06_strings/Exercise_06.py
+++ week_03/labs/06_strings/Exercise_06.py
@@ -17,3 +17,28 @@ and generalize it so that it accepts the string and the letter as arguments.
 it uses the three-parameter version of find from the previous section.
 
 '''
+def count(word, letter):
+    count = 0
+    for char in word:
+        if char == letter:
+            count += 1
+    return count
+
+
+def find(word, letter, index):
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index += 1
+    return -1
+
+def count_again(word, letter):
+    index, count = 0, 0
+    for i in range(len(word)):
+        index = find(word, letter, index)
+        if index == -1:  # if no match is found, break the loop
+            break
+        else:  # otherwise increment both
+            count += 1
+            index += 1  # becomes the new starting point for find()
+    return count
