eb167b419dc0502dc00bb9db3c59380e071742da
Patrikc Vargos <pvargos17@gmail.com>
2018-10-27 15:53:50 -0500

    completed

--- week_03/labs/06_strings/Exercise_05.py
+++ week_03/labs/06_strings/Exercise_05.py
@@ -6,3 +6,24 @@ and prints it out in the following three formats.
     - All vowels lower case and all consonants upper case.
 
 '''
+
+# def problem(str)
+
+user_str = input("Please enter something you asshat: ")
+
+capital = " "
+lower = " "
+vowel = "aeiou"
+vowels = " "
+
+for n in user_str:
+    if n.isupper():
+        capital += n
+    if n.islower():
+        lower+= n
+    if n in vowel:
+        vowels +=n
+print(capital)
+print(lower)
+print(vowels)
+
