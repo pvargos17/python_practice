d338df33fbabc3da347e24e8d564e0489892fc2c
Patrikc Vargos <pvargos17@gmail.com>
2018-10-29 18:02:19 -0500

    completed

--- week_03/labs/07_lists/Exercise_03.py
+++ week_03/labs/07_lists/Exercise_03.py
@@ -4,3 +4,13 @@ Complete Exercise 10.1 (p.120) from the textbook.
 Sum up all elements from a nested list of integers.
 
 '''
+
+t = [[1,2],[3],[4,5,6]]
+sum1 = 0
+
+for n in t:
+    sum1 += sum(n)
+print(sum1)
+
+
+
