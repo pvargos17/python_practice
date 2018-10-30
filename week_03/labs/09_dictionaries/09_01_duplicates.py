8e9a6c692338ba556bc0c7425d1e3d9ee7bec7b7
Patrikc Vargos <pvargos17@gmail.com>
2018-10-30 12:45:37 -0500

    completed

--- week_03/labs/09_dictionaries/09_01_duplicates.py
+++ week_03/labs/09_dictionaries/09_01_duplicates.py
@@ -9,3 +9,17 @@ Source: Chapter "Dictionaries" in Think Python 2e:
 http://greenteapress.com/thinkpython2/html/thinkpython2011.html
 
 '''
+
+
+def has_duplicates(list1):
+    d = dict()
+    for n in list1:
+        if n in d:
+            return "True"
+        else:
+            d[n] = 1
+    print(d)
+x = (1,2,3,4,5,5,6)
+y = (1,2,3,4,5,6)
+
+print(has_duplicates(y))
--- week_03/labs/09_dictionaries/09_02_combine_dicts.py
+++ week_03/labs/09_dictionaries/09_02_combine_dicts.py
@@ -8,3 +8,16 @@ Then print out each key-value pair.
 dict_1 = {1: 1, 2: 4}
 dict_2 = {3: 9, 4: 16}
 dict_3 = {5: 25, 6: 36, 7: 49}
+
+
+new_dict = {}
+
+for c in dict_1:
+    new_dict[c] = dict_1[c]
+    for d in dict_2:
+        new_dict[d] = dict_2[d]
+        for e in dict_3:
+            new_dict[e] = dict_3[e]
+for f in new_dict:
+    print(f , new_dict[f])
+
