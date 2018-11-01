7558b531a4d812e0a9dfaeab7e406f03a57ebcd6
Patrikc Vargos <pvargos17@gmail.com>
2018-10-31 20:56:34 -0500

    completed

--- week_03/labs/12_files/12_01_long_words.py
+++ week_03/labs/12_files/12_01_long_words.py
@@ -5,3 +5,8 @@ words with more than 20 characters (not counting whitespace).
 Source: http://greenteapress.com/thinkpython2/html/thinkpython2010.html
 
 '''
+fin = open('words.txt')
+# print(fin.readline())
+for line in fin:
+    if len(line) >= 20:
+        print(line)
